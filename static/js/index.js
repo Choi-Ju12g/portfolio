8729ae39075fa285b29ffd46ac424d3aa5b53425
Choi-Ju12g <choi-ju12g@naver.com>
2022-02-19 21:47:02 +0900

    html, and index.js which is router

--- static/js/index.js
+++ static/js/index.js
@@ -18,18 +18,22 @@ const router = async () => {
       
     let match = pageMatches.find((pageMatch) => pageMatch.isMatch);
     console.log(pageMatches);
+    console.log(match.route.view());
 }
 
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     document.body.addEventListener("click", (e) => {
-//         if (e.target.matches("[data-link]")) {
-//             e.preventDefault();
-//             history.pushState(null, null, e.target.href);
-//             router();
-//         }
-//     });
-//     router();
-// });
+// 뒤로 가기 할 때 데이터 나오게 하기 위함
+window.addEventListener("popstate", () => {
+    router();
+});
 
-router();
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", (e) => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            history.pushState(null, null, e.target.href);
+            router();
+        }
+    });
+    router();
+});
