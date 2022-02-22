8b85af7792890788354b9fa2087413bcbadddb96
Choi-Ju12g <choi-ju12g@naver.com>
2022-02-22 21:59:32 +0900

    make url form without express server to make GH pages

--- static/js/index.js
+++ static/js/index.js
@@ -4,27 +4,56 @@ import AboutMe from "./components/aboutme.js";
 import NotFound from "./components/NotFound.js";
 import Projects from "./components/projects.js"
 import RoadMap from "./components/roadmap.js";
-import History from "./components/history.js"
+import History from "./components/history.js";
+import { u } from "./lib.js";
+
+const pathToRegex = path =>
+  new RegExp('^' + path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)') + '$');
+
+const getParams = match => {
+  const values = match.result.slice(1);
+
+  const keys = Array.from(
+    match.route.path
+      .matchAll(/:(\w+)/g))
+      .map(result => result[1]
+  );
+
+  return Object.fromEntries(
+    keys.map((key, i) => {
+      return [
+        key,
+        values[i]
+      ];
+    })
+  );
+};
+
+const navigateTo = url => {
+  history.pushState(null, null, url);
+  router();
+};
 
 const router = async () => {
     const routes = [
-    {path:'/', view: Home},
-    {path:'/aboutme', view: AboutMe},       
-    {path:'/skills', view :Skills},
-    {path:'/projects', view: Projects},
-    {path:'/roadmap', view:RoadMap},       
-    {path:'/history', view:History},        
+    {path:u('/'), view: Home},
+    {path:u('/aboutme'), view: AboutMe},       
+    {path:u('/skills'), view :Skills},
+    {path:u('/projects'), view: Projects},
+    {path:u('/roadmap'), view:RoadMap},       
+    {path:u('/history'), view:History},        
     ];
 
     const pageMatches = routes.map((route) => {
         return {
           route: route,
-          isMatch: location.pathname === route.path,
+          //isMatch: location.pathname === route.path,
+          isMatch: location.pathname.match(pathToRegex(route.path))
         };
       });  
     
       
-    let match = pageMatches.find((pageMatch) => pageMatch.isMatch);
+    let match = pageMatches.find((pageMatch) => pageMatch.isMatch !== null);
     // console.log(pageMatches);
     // console.log(match.route.view());
     if(!match){
