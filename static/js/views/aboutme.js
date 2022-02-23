beb4c2a57ccfc1056354c76995d054794b5845fd
Choi-Ju12g <choi-ju12g@naver.com>
2022-02-23 17:07:59 +0900

    add img directory

--- static/js/views/aboutme.js
+++ static/js/views/aboutme.js
@@ -1,5 +1,6 @@
 import AbstractView from './AbstractView.js';
 import { u } from "../lib.js";
+import porfile from "../img/porfile.jpg"
 
 export default class extends AbstractView {
     constructor(params) {
@@ -9,6 +10,7 @@ export default class extends AbstractView {
     
     async getHtml() {
         return `
+            <img src="./img/profile.jpg" alt="My profile" width="200" height="200">
             <h1>This is About_Me Page</h1>
         `;
     }
